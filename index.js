17da13d07bb54d33c8bd9926fd619a84db208826
alex <alexmiller.ml@gmail.com>
2020-07-08 12:52:17 -0400

    use sockets so each game is in a room

--- index.js
+++ index.js
@@ -6,92 +6,101 @@ var port = process.env.PORT || 5000;
 
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false
-})
-
-/*
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'snatch',
-  password: 'postgres',
-  port: 5432,
-  ssl: false
-});
-*/
-
-pool.connect();
-
-// TODO make rooms based on game id
-gameId = "103";
-
-const buildFlipQuery = (data, gameId) => {
-  squares = Object.values(data['squares'])
-      .map((s) => JSON.stringify(s)).join(",");
-  return "UPDATE game SET squares = '{" +
-    squares + "}' WHERE id = " + gameId
-}
-
-const buildSnatchQuery = (data, gameId) => {
-  squares = Object.values(data['squares'])
-      .map((s) => JSON.stringify(s)).join(",");
-  p1words = data['p1words']
-      .map((s) => JSON.stringify(s)).join(",");
-  p2words = data['p2words']
-      .map((s) => JSON.stringify(s)).join(",");
-  return "UPDATE game SET squares = '{" + squares +
-    "}', p1words = '{" + p1words + "}', p2words = '{" +
-    p2words + "}' WHERE id = " + gameId
-}
-
-const buildStateQuery = (gameId) => {
-  return "SELECT * FROM game WHERE id = " + gameId
-}
-
+/* ---------- Routes ------------------- */
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
 
 app.get('/get_wordlist', (req, res) => {
-  console.log("in get wordlist");
   const words = fs.readFileSync("./sowpods.txt", "utf8").split("\n");
-  console.log("words length: " + words.length);
   res.send({ data: words });
 });
 
-app.get('/check_db', (req, res) => {
-  pool.query("SELECT * FROM game")
-    .then(res => console.log(res.rows[0]))
-    .catch(e => console.error(e.stack))
+/* ----------Postgres connection logic--------- */
+const prod = false;
+let pool = undefined;
+if (prod) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
+  })
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'snatch',
+    password: 'postgres',
+    port: 5432,
+    ssl: false
+  });
+}
+
+async function getData(room) {
+  try {
+    response = await pool.query("SELECT * FROM game WHERE id = " + room);
+  } catch (error) {
+    console.error(e.stack)
+  }
+  return response.rows[0];
+}
+
+async function create(room) {
+  try {
+    response = await pool.query(
+      "INSERT INTO game (id) VALUES (" + room + ") ON CONFLICT DO NOTHING"
+    )
+  } catch (error) {
+    console.error(e.stack)
+  }
+}
+
+const buildFlipQuery = (data) => {
+  squares = Object.values(data['squares']).map((s) => JSON.stringify(s)).join(",");
+  return "UPDATE game SET squares = '{" + squares + "}' WHERE id = " + data['room']
+}
+
+const buildSnatchQuery = (data) => {
+  squares = Object.values(data['squares']).map((s) => JSON.stringify(s)).join(",");
+  p1words = data['p1words'].map((s) => JSON.stringify(s)).join(",");
+  p2words = data['p2words'].map((s) => JSON.stringify(s)).join(",");
+  return "UPDATE game SET squares = '{" + squares +
+    "}', p1words = '{" + p1words + "}', p2words = '{" +
+    p2words + "}' WHERE id = " + data['room']
+}
+
+/* ------------------------------------------- */
+
+io.sockets.on('connection', function(socket){
+  socket.on('join-send', function(data) {
+    socket.join(data['room']);
   });
 
-io.on('connection', function(socket){
-  pool
-    .query(buildStateQuery(gameId))
-    .then(res => io.emit('client-connect-receive', res.rows[0]) )
-    .catch(e => console.error(e.stack))
+  socket.on('get-state-send', async (data) => {
+    const room = data['room']
+    await create(room);
+    const rows = await getData(room)
+    io.sockets.in(room).emit('client-connect-receive', rows) 
+  })
 
   socket.on('flip-send', function(data){
     pool
-      .query(buildFlipQuery(data, gameId))
+      .query(buildFlipQuery(data))
       .catch(e => console.error(e.stack))
-    io.emit('flip-receive', data);
+    io.sockets.in(data['room']).emit('flip-receive', data);
   });
 
   socket.on('snatch-send', function(data){
     pool
-      .query(buildSnatchQuery(data, gameId))
+      .query(buildSnatchQuery(data))
       .catch(e => console.error(e.stack))
-    io.emit('snatch-receive', data);
+    io.sockets.in(data['room']).emit('snatch-receive', data);
   });
 
-  socket.on('reset-send', () => {
-    const data = {'squares': {}, 'p1words': [], 'p2words': []}
+  socket.on('reset-send', (room) => {
+    const data = {'squares': {}, 'p1words': [], 'p2words': [], 'room': room}
     pool
-      .query(buildSnatchQuery(data, gameId))
+      .query(buildSnatchQuery(data))
       .catch(e => console.error(e.stack))
-    io.emit('reset-receive');
+    io.sockets.in(data['room']).emit('reset-receive');
   });
-});
+})
